df9edd5107a7b3f656fb0d96f5801addd80fe445
Saharsh365 <67753821+Saharsh365@users.noreply.github.com>
2024-12-15 22:09:19 -0500

    Insert method

--- rehabReady.js
+++ rehabReady.js
@@ -10,12 +10,15 @@ const uri = process.env.MongoDB;
 
 const client = new MongoClient(uri);
 
-async function insertApplicant() {
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+async function insertApplicant(name, injury) {
   try {
     const myDB = client.db("rehabReady");
     const myColl = myDB.collection("users");
 
-    const doc = { name: "Saharsh Maloo", injury: "tendonitis" };
+    const doc = { name: name, injury: injury };
     const result = await myColl.insertOne(doc);
     console.log(
     `A document was inserted with the _id: ${result.insertedId}`,
@@ -32,6 +35,29 @@ app.set('views', './templates');
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
+
+app.get('/insert', (req, res) => {
+    res.render('insert.ejs')
+})
+
+app.get('/results', (req, res) => {
+    res.render('results.ejs')
+})
+
+app.post('/insertApplicant', async (req, res) => {
+    const { name, injury } = req.body;
+    if (!name || !injury) {
+      return res.status(400).send("Name and injury fields are required!");
+    }
+  
+    try {
+      await insertApplicant(name, injury);
+      res.status(200);
+    } catch (err) {
+      console.error("Error during POST /add-applicant:", err);
+      res.status(500);
+    }
+  });
 
 app.listen(portNumber, () => {
     console.log(`Try: http://localhost:${portNumber}`);
