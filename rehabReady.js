2fb084e9e6107e9c453064b12b564000d3b03c0c
Saharsh365 <67753821+Saharsh365@users.noreply.github.com>
2024-12-16 00:17:32 -0500

    db setup

--- rehabReady.js
+++ rehabReady.js
@@ -14,23 +14,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 async function insertApplicant(name, injury) {
-  try {
     const myDB = client.db("rehabReady");
     const myColl = myDB.collection("users");
-
     const doc = { name: name, injury: injury };
     const result = await myColl.insertOne(doc);
     console.log(
     `A document was inserted with the _id: ${result.insertedId}`,
     );
-  } finally {
-    await client.close();
-  }
 }
-// run().catch(console.dir);
+
+async function getInjuries(name) {
+    const myDB = client.db("rehabReady");
+    const myColl = myDB.collection("users");
+    const cursor = await myColl.find(name);
+    return cursor;
+}
+
+async function removeAll() {
+    let result;
+    try {
+        await client.connect();
+        console.log("***** Clearing Collection *****");
+        const myDB = client.db("rehabReady");
+        const myColl = myDB.collection("users");
+        result = await myColl.deleteMany({});
+        console.log(`Deleted documents ${result.deletedCount}`);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 
 app.set('view engine', 'ejs');
 app.set('views', './templates');
+app.use(express.static('styles'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -52,12 +69,29 @@ app.post('/insertApplicant', async (req, res) => {
   
     try {
       await insertApplicant(name, injury);
-      res.status(200);
+      res.status(200).redirect('/results');
     } catch (err) {
-      console.error("Error during POST /add-applicant:", err);
-      res.status(500);
+      console.error("Error during POST /insertApplicant:", err);
+      res.status(500).send("Error during POST /insertApplicant:");
     }
-  });
+});
+
+app.post('/showResults', async (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    if (!name) {
+      return res.status(400).send("Name is required!");
+    }
+  
+    let dbResult = await getInjuries();
+    for await (const doc of dbResult) {
+        console.dir(doc);
+      }
+});
+
+app.post('/delete', async (req, res) => {
+    await removeAll();
+});
 
 app.listen(portNumber, () => {
     console.log(`Try: http://localhost:${portNumber}`);
