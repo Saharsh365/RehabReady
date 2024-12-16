f185451139db29c6321663267f8fb0f6298c866e
Saharsh365 <67753821+Saharsh365@users.noreply.github.com>
2024-12-15 20:10:37 -0500

    Mongo setup

--- rehabReady.js
+++ rehabReady.js
@@ -1,9 +1,36 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
 const portNumber = 3000
 
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = process.env.MongoDB;
+
+const client = new MongoClient(uri);
+
+async function insertApplicant() {
+  try {
+    const myDB = client.db("rehabReady");
+    const myColl = myDB.collection("users");
+
+    const doc = { name: "Saharsh Maloo", injury: "tendonitis" };
+    const result = await myColl.insertOne(doc);
+    console.log(
+    `A document was inserted with the _id: ${result.insertedId}`,
+    );
+  } finally {
+    await client.close();
+  }
+}
+// run().catch(console.dir);
+
+app.set('view engine', 'ejs');
+app.set('views', './templates');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index.ejs')
 })
 
 app.listen(portNumber, () => {
